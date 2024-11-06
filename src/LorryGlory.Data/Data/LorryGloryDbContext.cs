5b2550a06a6f05b78fb7de019510e6ee3468a39f
PAhlback <pontus.ahlback93@gmail.com>
2024-11-06 14:03:29 +0100

    Add database configuration

---
+++ src/LorryGlory.Data/Data/LorryGloryDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LorryGlory.Data.Data
+{
+    public class LorryGloryDbContext : IdentityDbContext
+    {
+        public LorryGloryDbContext(DbContextOptions options) :base(options) { }
+
+
+    }
+}
