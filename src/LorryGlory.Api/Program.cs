09b0d31ddba7a99c057c73df51fd92f192bbdb6f
Madagasikara <m66@o2.pl>
2024-11-15 16:01:31 +0100

    Identity Auth&Auth with MapCustomIdentityApi. Register works so far.

--- src/LorryGlory.Api/Program.cs
+++ src/LorryGlory.Api/Program.cs
@@ -1,4 +1,7 @@
 ﻿using LorryGlory.Core.Configuration;
+using LorryGlory.Data.Data;
+using LorryGlory.Data.Models.StaffModels;
+using Microsoft.AspNetCore.Identity;
 
 namespace LorryGlory.Api
 {
@@ -20,11 +23,34 @@ namespace LorryGlory.Api
             builder.Services.ConfigureDatabase(connectionString);
             builder.Services.ConfigureScopes();
 
+            builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
+                .AddIdentityCookies();
+            builder.Services.AddAuthorizationBuilder();
+// OBS ska den och andra till ServiceConfiguration??
+            builder.Services.AddIdentityCore<StaffMember>()
+               .AddEntityFrameworkStores<LorryGloryDbContext>()
+               .AddApiEndpoints();
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 1;
+                options.Password.RequiredUniqueChars = 0;
+
+                // Email settings.
+                options.SignIn.RequireConfirmedEmail = false;
+            });
+
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+            
+            app.MapGroup("auth/").MapCustomIdentityApi<StaffMember>();
 
             // Configure the HTTP request pipeline
             if (app.Environment.IsDevelopment())
--- src/LorryGlory.Core/Services/VehicleService.cs
+++ src/LorryGlory.Core/Services/VehicleService.cs
@@ -1,4 +1,5 @@
-﻿using LorryGlory.Core.Services.IServices;
+﻿using LorryGlory.Core.Models.DTOs;
+using LorryGlory.Core.Services.IServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace LorryGlory.Core.Services
 {
-    public class VehicleService:IVehicleService
+    public class VehicleService : IVehicleService
     {
+        public Task<VehicleDto> CreateAsync(VehicleDto vehicleDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VehicleDto> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<VehicleDto>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<VehicleDto>> GetAllByDriverIdAndDayAsync(int id, DateOnly date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VehicleDto> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VehicleDto> GetByRegNoAsync(string regNo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VehicleDto> UpdateAsync(VehicleDto vehicleDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
