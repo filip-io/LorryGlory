b1410a60bcee1bb42c716ee1ca798f6f8a82cd70
filip-io <filip.n@outlook.com>
2024-11-17 22:14:58 +0100

    Update DTOs
    - AddressDto
    - ContactPersonDto
    - JobTaskDto

--- src/LorryGlory.Core/Models/DTOs/ContactPersonDto.cs
+++ src/LorryGlory.Core/Models/DTOs/ContactPersonDto.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace LorryGlory.Core.Models.DTOs
 {
     public class ContactPersonDto
     {
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+
+    public class ContactPersonCreateUpdateDto
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        [StringLength(255)]
+        public string? Email { get; set; }
+
+        [Phone]
+        [StringLength(20)]
         public string? PhoneNumber { get; set; }
     }
 }
--- src/LorryGlory.Core/Models/DTOs/JobTaskDto.cs
+++ src/LorryGlory.Core/Models/DTOs/JobTaskDto.cs
@@ -1,47 +1,104 @@
-﻿using LorryGlory.Data.Models.JobModels.Enums;
-using LorryGlory.Data.Models.StaffModels.Enums;
+﻿using LorryGlory.Data.Models.CompanyModels;
+using LorryGlory.Data.Models.JobModels.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace LorryGlory.Core.Models.DTOs
 {
     public class JobTaskDto
     {
         public Guid Id { get; set; }
-        public string? Title { get; set; }
+        public string Title { get; set; }
         public string? Description { get; set; }
         public JobTaskStatus Status { get; set; }
         public bool IsCompleted { get; set; }
-
-        // ContactPerson
         public ContactPersonDto? ContactPerson { get; set; }
-
-        // Addresses
         public AddressDto? PickupAddress { get; set; }
         public AddressDto? DeliveryAddress { get; set; }
-
-        // Timestamps
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
+        public string? FK_StaffMemberId { get; set; }
+        public StaffMemberDto? StaffMember { get; set; }
+        public Guid FK_JobId { get; set; }
+        public JobDto Job { get; set; }
+        public Guid? FK_VehicleId { get; set; }
+        public VehicleDto? Vehicle { get; set; }
+        public Guid? FK_FileLink { get; set; }
+        public FileLinkDto? FileLink { get; set; }
+        public JobTaskReportDto? JobTaskReport { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+        public Guid FK_TenantId { get; set; }
+        public Company Company { get; set; }
+    }
 
-        // Staff Member Info
-        public string? StaffMemberId { get; set; }
-        public StaffMemberDto? StaffMember { get; set; }
+    public class JobTaskCreateDto
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Title { get; set; }
 
-        // Job Info
-        public JobDto Job { get; set; }
+        [StringLength(500)]
+        public string? Description { get; set; }
 
-        // Vehicle Info (optional)
-        public VehicleDto? Vehicle { get; set; }
+        public JobTaskStatus Status { get; set; } = JobTaskStatus.New;
 
-        // File Link (optional)
-        public FileLinkDto? FileLink { get; set; }
+        public bool IsCompleted { get; set; } = false;
 
-        // Report
-        public JobTaskReportDto? JobTaskReport { get; set; }
+        public ContactPersonCreateUpdateDto? ContactPerson { get; set; }
 
-        // Company/Tenant Info
-        public Guid TenantId { get; set; }
-        public string? CompanyName { get; set; }
+        [Required]
+        public AddressCreateUpdateDto PickupAddress { get; set; }
+
+        [Required]
+        public AddressCreateUpdateDto DeliveryAddress { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        public DateTime EndTime { get; set; }
+
+        public string? FK_StaffMemberId { get; set; }
+
+        [Required]
+        public Guid FK_JobId { get; set; }
+
+        public Guid? FK_VehicleId { get; set; }
+
+        public Guid? FK_FileLink { get; set; }
+    }
+
+    public class JobTaskUpdateDto
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public JobTaskStatus Status { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public ContactPersonCreateUpdateDto? ContactPerson { get; set; }
+
+        [Required]
+        public AddressCreateUpdateDto PickupAddress { get; set; }
+
+        [Required]
+        public AddressCreateUpdateDto DeliveryAddress { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        public DateTime EndTime { get; set; }
+
+        public string? FK_StaffMemberId { get; set; }
+
+        public Guid? FK_VehicleId { get; set; }
+
+        public Guid? FK_FileLink { get; set; }
     }
 }
