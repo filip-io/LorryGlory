7cc85c7f099de20b69813d5a15059c3f2dd388a8
Madagasikara <m66@o2.pl>
2024-11-20 16:09:10 +0100

    created co-repo, removed gettenants

---
+++ src/LorryGlory.Data/Repositories/CompanyRepository.cs
@@ -0,0 +1,38 @@
+﻿using LorryGlory.Data.Models.CompanyModels;
+using LorryGlory.Data.Repositories.IRepositories;
+
+namespace LorryGlory.Data.Repositories
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        public Task<Company> CreateAsync(Company company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company?> DeleteAsync(Company company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Company?>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company?> GetByOrganizationNumberAsync(string organizationNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company?> UpdateAsync(Company company)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
