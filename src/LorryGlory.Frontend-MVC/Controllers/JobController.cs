ddb0b8e326778ca92cece9ab5f52b4ac9955a4c4
SurrealEmil <emil.ejderklev1@hotmail.com>
2024-11-20 15:27:46 +0100

    Feeling stuck
    Pausing this to work on other parts of the project so i dont sitt here doing nothing

---
+++ src/LorryGlory.Frontend-MVC/Controllers/JobController.cs
@@ -0,0 +1,19 @@
+﻿using LorryGlory_Frontend_MVC.ViewModels.JobViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LorryGlory_Frontend_MVC.Controllers
+{
+    public class JobController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult JobCreate()
+        {
+
+            return View(new CreateJobViewModel());
+        }
+    }
+}
---
+++ src/LorryGlory.Frontend-MVC/Controllers/TodaysMissionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LorryGlory_Frontend_MVC.ViewModels;
+
+namespace LorryGlory_Frontend_MVC.Controllers
+{
+    public class TodaysMissionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TaskCreate()
+        {
+
+            return View(new CreatetaskViewModel());
+        }
+    }
+}
