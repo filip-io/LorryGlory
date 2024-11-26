1b47cd8bacbcfadeedfa432b36c5e741e430164e
Madagasikara <m66@o2.pl>
2024-11-26 02:16:07 +0100

    Authorize menu

--- src/LorryGlory.Frontend-MVC/Controllers/MenuController.cs
+++ src/LorryGlory.Frontend-MVC/Controllers/MenuController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LorryGlory_Frontend_MVC.Controllers
 {
     public class MenuController : Controller
     {
+        [Authorize]
         public IActionResult Index()
         {
             return View();
