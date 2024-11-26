c30ef709dc01fed42566a01e25b763ccbd240c69
SurrealEmil <emil.ejderklev1@hotmail.com>
2024-11-26 09:58:32 +0100

    Add Automaper for role

--- src/LorryGlory.Api/Helpers/AutoMapperProfile.cs
+++ src/LorryGlory.Api/Helpers/AutoMapperProfile.cs
@@ -25,6 +25,7 @@ namespace LorryGlory.Api.Helpers
             CreateMap<IdentityRole, IdentityRoleDto>().ReverseMap();
             CreateMap<IdentityRole, IdentityRoleCreateDto>().ReverseMap();
             CreateMap<IdentityRole, IdentityRoleUpdateDto>().ReverseMap();
+            CreateMap<IdentityResult, IdentityRoleDto>().ReverseMap();
 
             // StaffMember mappings
             CreateMap<StaffMember, StaffMemberDto>().ReverseMap();
